545b0e68d1de28f6fe36f6971001561072c36b94
Andrey Morozov <pisemest@yandex.ru>
2023-03-27 15:37:57 +0500

    Simple console app v2

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+(function() {
+    const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+    if (typeof numberOfFilms === "number"){
+        const personalMovieDB = {
+            count: numberOfFilms,
+            movies: {},
+            actors: {},
+            genres: [],
+            privat: false
+        }
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+                b = prompt('На сколько его оцените?');
+            if (a !== null && b !== null && a !== '' && b !== '' && a.length < 50 && b.length < 50) {
+                personalMovieDB.movies[a] = b
+            } else {
+                i--;
+            }
+        }
+        if (personalMovieDB.count < 10) {
+            console.log('Просмотрено довольно мало фильмов')
+        }
+        if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+            console.log('Вы классический зритель')
+        }
+        if (personalMovieDB.count > 30) {
+            console.log('Вы киноман')
+        }
+        console.log(personalMovieDB)
+    } else {
+        console.log('error')
+    }
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-}
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько его оцените?'),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько его оцените?');
+})()
 
-personalMovieDB.movies[a] = b
-personalMovieDB.movies[c] = d
-console.log(personalMovieDB)
